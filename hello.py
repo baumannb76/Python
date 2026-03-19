959d3ed2c4340c71e63883aa28b7fd669e2993da
Bryan <baumannb76@gmail.com>
2026-03-19 16:53:15 -0600

    new file:   decade.py 	modified:   hello.py 	new file:   taxCalc.py

--- hello.py
+++ hello.py
@@ -1,1 +1,11 @@
-print("Hello World")
+hello = 'Hello'
+
+name = input("What's your name?\n")
+
+# Print option 1 with variable
+greeting = hello + " " + name
+print(greeting)
+
+#Print option 2 with direct variables
+print(hello + " " + name)
+
