1d36bc9451a3b8dd99d977978c350db0a1796707
Bryan <baumannb76@gmail.com>
2026-03-19 21:30:05 -0600

    new file:   weather.py

---
+++ weather.py
@@ -0,0 +1,10 @@
+temp = 95
+
+if temp > 80:
+    print("It's too damn hot")
+    print("Stay inside!")
+elif temp < 60:
+    print("It's too damn cold")
+    print("Stay inside!")    
+else:
+    print("Enjoy the outdoors")
