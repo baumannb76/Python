8bb8a6eeb43c7fd452c96ea5d73cc7bb85cb8096
Bryan <baumannb76@gmail.com>
2026-03-19 22:02:32 -0600

    new file:   rockPaperScissors.py

---
+++ rockPaperScissors.py
@@ -0,0 +1,16 @@
+import random
+
+comp_choice = random.choice(['rock', 'paper', 'scissors'])
+
+user_choice = input('Do you want Rock, Paper or Scissors?')
+
+if comp_choice == user_choice:
+    print('TIE')
+elif user_choice == 'rock' and comp_choice == 'scissors':
+    print('WIN')
+elif user_choice == 'paper' and comp_choice == 'rock':
+    print('WIN')
+elif user_choice == 'scissors' and comp_choice == 'paper':
+    print('WIN')
+else:
+    print('You lose')
